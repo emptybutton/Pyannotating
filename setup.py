3ec4f22abc92a75102f35d6f85ec138923e6469b
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-05 20:17:49 +0700

    Installation file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+PACKAGE_NAME = 'pyannotating'
+
+VERSION = '1.0.0'
+
+with open('README.md') as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
+
+setup(
+    name=PACKAGE_NAME,
+    description="Library for convenient generation of annotations for your code",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license_files = ('LICENSE',),
+    license="GNU General Public License v3.0",
+    version=VERSION,
+    url="https://github.com/TheArtur128/Pyannotating",
+    download_url=f"https://github.com/TheArtur128/Pyannotating/archive/refs/tags/v{VERSION}.zip",
+    author="Arthur",
+    author_email="s9339307190@gmail.com",
+    python_requires='>=3.11',
+    classifiers=["Programming Language :: Python :: 3.11"],
+    keywords=['library', 'annotations' 'generation'],
+    py_modules=[PACKAGE_NAME]
+)
