ee3bcbc6274541ff6cdefa89d7164a98efc21116
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-05 14:40:34 +0700

    AnnotationFactory class delegating the construction to another factory

--- named_annotations.py
+++ named_annotations.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Iterable, Mapping, Final
 
 
 class AnnotationFactory(ABC):
@@ -23,4 +24,65 @@ class AnnotationFactory(ABC):
     def _create_full_annotation_by(self, annotation: any) -> any:
         """Annotation Creation Method from an input annotation."""
 
+
+class CustomAnnotationFactory(AnnotationFactory):
+    """
+    AnnotationFactory class delegating the construction of another factory's
+    annotation.
+
+    When called, replaces the 'annotation' strings from its arguments and their
+    subcollections with the input annotation.
+    """
+
+    _input_annotation_annotation: str = '<input_annotation>'
+
+    def __init__(self, original_factory: Mapping, annotations: Iterable):
+        self._original_factory = original_factory
+        self._annotations = tuple(annotations)
+
+    @property
+    def original_factory(self) -> Mapping:
+        return self._original_factory
+
+    @property
+    def annotations(self) -> tuple:
+        return self._annotations
+
+    @classmethod
+    @property
+    def input_annotation_annotation(cls) -> str:
+        return cls._input_annotation_annotation
+
+    def __repr__(self) -> str:
+        return "{factory}{arguments}".format(
+            factory=(
+                self._original_factory.__name__
+                if hasattr(self._original_factory, '__name__')
+                else self._original_factory
+            ),
+            arguments=str(self.__recursively_format(self._annotations)).replace('\'', str())
+        )
+
+    def _create_full_annotation_by(self, annotation: any) -> any:
+        return self._original_factory[
+            *self.__get_formatted_annotations_from(self._annotations, annotation)
+        ]
+
+    def __recursively_format(self, collection: Iterable) -> list:
+        """
+        Method for formatting the elements of a collection (and all of its
+        sub-collections) as a list with possible element names or themselves.
+        """
+
+        formatted_collection = list()
+
+        for item in collection:
+            if isinstance(item, Iterable) and not isinstance(item, str):
+                formatted_collection.append(self.__recursively_format(item))
+            elif hasattr(item, '__name__'):
+                formatted_collection.append(item.__name__)
+            else:
+                formatted_collection.append(item)
+
+        return formatted_collection
 
